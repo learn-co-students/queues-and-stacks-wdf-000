35a8e61d3edca8c26607fd23c8ca8a17e319dc7f
ozPop <ozziepopovas@gmail.com>
2016-12-05 14:32:05 -0500

    Done.

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,17 @@
 class Stack
+  def initialize
+    @stack = []
+  end
 
+  def push(value)
+    @stack.push(value)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def size
+    @stack.length
+  end
 end
