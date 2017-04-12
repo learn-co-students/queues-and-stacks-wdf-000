2a5eaa65f50404cf0e1df3561e27cf86ebba2b6b
sami4thewin <samiomarmohammed@gmail.com>
2017-04-12 01:29:44 -0400

    Done.

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,4 +2,22 @@ require_relative 'stack'
 
 class Queue
 
+  def initialize
+    @stack1 = Stack.new
+    @stack2 = Stack.new
+  end
+
+  def enqueue(item)
+    @stack1.push(item)
+  end
+
+  def dequeue
+    if @stack2.size == 0
+      while @stack1.size > 0
+        @stack2.push(@stack1.pop)
+      end
+    end
+    @stack2.pop
+  end
+
 end
